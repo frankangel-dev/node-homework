86c11dd0a6fe781cfbff4f2db07a8dd2dd0e2dc0
Frank <205842738+frankangel-dev@users.noreply.github.com>
2026-08-07 06:32:52 -0700

    Add reCAPTCHA verification to user registration

--- test/user.function.test.js
+++ test/user.function.test.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const request = require("supertest");
 process.env.DATABASE_URL = process.env.TEST_DATABASE_URL;
+process.env.RECAPTCHA_BYPASS = process.env.JWT_SECRET;
 const prisma = require("../db/prisma");
 const { app, server } = require("../app");
 
@@ -27,7 +28,10 @@ describe("register a user", () => {
       email: "jdeere@example.com",
       password: "Pa$$word20",
     };
-    saveRes = await agent.post("/api/users/register").send(newUser);
+    saveRes = await agent
+      .post("/api/users/register")
+      .set("X-Recaptcha-Test", process.env.JWT_SECRET)
+      .send(newUser);
     expect(saveRes.status).toBe(201);
   });
 
